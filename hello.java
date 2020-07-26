5b467738884306dfaca992194f90704d1787249d
Rasel Amin <raselamin04081999@gmail.com>
2020-07-27 00:13:04 +0600

    added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(string[]args){
         system.out.println("vs code is awesome...");
+
+        greeting("Rasel");
+    }
+
+    public static void greeting(string name){
+        system.out.println("welcome"+ name);
     }
 }

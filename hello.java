58b8fafaec08469d0dc270104d7dd6541c0f3714
Rasel Amin <raselamin04081999@gmail.com>
2020-07-27 01:39:09 +0600

    all method remove and add message method

--- hello.java
+++ hello.java
@@ -2,10 +2,11 @@ public class hello {
     public static void main(string[]args){
         system.out.println("vs code is awesome...");
 
-      display();
+    
     }
-
-    public static void display(){
-        system.out.println("hello hello");
+    public static void message(){
+        system.out.println("hooray!!! Now we are able to contribute an open source....")
+    }
+    
     }
 }
